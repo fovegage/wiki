3e3647770f8848ad0a10618c459765b267f35a23
fovegage <fovegage@gmail.com>
2023-06-09 16:31:53 +0800

    feat: update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,12 +5,12 @@ import {defineConfig4CustomTheme, UserPlugins} from 'vuepress/config'
 import {VdoingThemeConfig} from 'vuepress-theme-vdoing/types'
 // @ts-ignore
 import dayjs from 'dayjs'
-import baiduCode from './config/baiduCode' // 百度统计hm码
 import htmlModules from './config/htmlModules' // 自定义插入的html块
 
 const DOMAIN_NAME = 'gaozhe.net' // 域名 (不带https)
 const WEB_SITE = `https://${DOMAIN_NAME}` // 网址
 
+// @ts-ignore
 export default defineConfig4CustomTheme<VdoingThemeConfig>({
     theme: 'vdoing', // 使用npm主题包
     // theme: resolve(__dirname, '../../vdoing'), // 使用本地主题包
@@ -22,7 +22,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
             description: 'web前端技术博客,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,React,python,css3,html5,Node,git,github等技术文章。',
         }
     },
-    // base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
+    // base: './', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
 
     // 主题配置
     themeConfig: {
@@ -360,3 +360,6 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         '.vuepress/config/htmlModules.ts',
     ]
 })
+
+// @ts-ignore
+defineConfig4CustomTheme.base = './'
