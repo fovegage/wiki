05da48e01a4f4dd7ef1669420f7c8a22fb72c576
fovegage <fovegage@gmail.com>
2023-06-14 10:12:44 +0800

    feat: update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -250,7 +250,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         [
             'vuepress-plugin-baidu-tongji', // 百度统计
             {
-                hm: '503f098e7e5b3a5b5d8c5fc2938af002',
+                hm: '23e56b8f6103dd7a2a49bb5ce6500bcd',
             },
         ],
 
--- docs/.vuepress/config/baiduCode.ts
+++ docs/.vuepress/config/baiduCode.ts
@@ -1,1 +1,1 @@
-export default '503f098e7e5b3a5b5d8c5fc2938af002'
+export default '23e56b8f6103dd7a2a49bb5ce6500bcd'
