d902a012cd150fcd4a6b1398b42881e5f063ef51
fovename <fovegage@gmail.com>
2025-06-10 12:11:54 +0800

    auto deploy, remotes/origin/HEAD-0-gf511e5d

---
+++ assets/js/282.62cc1df6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[282],{629:function(t,n,o){"use strict";o.r(n);var s=o(15),e=Object(s.a)({},(function(){var t=this._self._c;return t("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[t("blockquote",[t("p",[this._v("目前采取 nano + qemu 的形式进行部署")])])])}),[],!1,null,null,null);n.default=e.exports}}]);
